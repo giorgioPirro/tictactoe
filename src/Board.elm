028c7fdfc395198a09d1e11f1b2faa7bcafeda17
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-05-29 19:10:28 +0100

    Board can hold marks

--- src/Board.elm
+++ src/Board.elm
@@ -1,16 +1,27 @@
-module Board exposing (Size(..), create, toList)
+module Board exposing (Size(..), Mark(..), create, markCell, toList, toArray)
 
 import Array exposing (Array)
 
 type Size  = Standard
 type Mark  = X | O
 type Board = Board (Array (Maybe Mark))
+type alias Position = Int
+type alias Move = (Position, Mark)
 
 create : Size -> Board
 create size =
     case size of
         Standard -> Board (Array.repeat 9 Nothing)
 
+markCell : Move -> Board -> Board
+markCell (position, mark) (Board cells) =
+    Array.set position (Just mark) cells
+        |> Board
+
 toList : Board -> List (Maybe Mark)
 toList (Board marks) =
     Array.toList marks
+
+toArray : Board -> Array (Maybe Mark)
+toArray (Board marks) =
+    marks
--- tests/BoardTest.elm
+++ tests/BoardTest.elm
@@ -2,8 +2,9 @@ module BoardTest exposing (boardTests)
 
 import Test exposing (Test, describe, test)
 import Expect
+import Array exposing (get)
 
-import Board exposing (Size(..), create, toList)
+import Board exposing (Size(..), Mark(..), create, toList, toArray)
 
 
 boardTests : Test
@@ -22,4 +23,26 @@ boardTests =
                       |> Board.toList
                       |> List.all (\cell -> cell == Nothing)
                       |> Expect.true "all cells should be empty to begin with"
+
+        , test "add the given mark at the given position (example one)" <|
+              \() ->
+                  let
+                      move = (7, X)
+                  in
+                      Board.create Standard
+                          |> Board.markCell move
+                          |> Board.toArray
+                          |> Array.get 7
+                          |> Expect.equal (Just (Just X))
+
+        , test "add the given mark at the given position (example two)" <|
+              \() ->
+                  let
+                      move = (5, O)
+                  in
+                      Board.create Standard
+                          |> Board.markCell move
+                          |> Board.toArray
+                          |> Array.get 5
+                          |> Expect.equal (Just (Just O))
         ]
