60c4c406e5e808a0a76471f2ad977fa1d768481b
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-05-30 17:32:26 +0100

    Add tieGame test helper

--- tests/Helpers.elm
+++ tests/Helpers.elm
@@ -1,8 +1,16 @@
-module Helpers exposing (createNewGame)
+module Helpers exposing (createNewGame, createTieGameStandardSizedBoard)
 
-import Board exposing (Size(..), Mark)
-import Game exposing (Game, Player)
+import Board exposing (Size(..), Mark(..))
+import Game exposing (Game, Player(..))
 
 createNewGame : Size -> (Player, Player) -> Game
 createNewGame size players =
     Game.create players (Board.create size)
+
+createTieGameStandardSizedBoard : Game
+createTieGameStandardSizedBoard =
+    let
+        newGame = Game.create (Human X, Human O) (Board.create Standard)
+        moves = [1, 0, 4, 2, 5, 3, 6, 7, 8]
+    in
+        List.foldl Game.makeMove newGame moves
