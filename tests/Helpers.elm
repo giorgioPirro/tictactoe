e2d9df4e3ad6a7d4624a3fbe0e4320ced7b73928
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-05-30 17:54:12 +0100

    Add createDrawBoard helper

--- tests/Helpers.elm
+++ tests/Helpers.elm
@@ -1,6 +1,7 @@
-module Helpers exposing (createNewGame, createTieGameStandardSizedBoard)
+module Helpers exposing (createNewGame, createTieGameStandardSizedBoard,
+                         createDrawBoardStandardSized)
 
-import Board exposing (Size(..), Mark(..))
+import Board exposing (Size(..), Mark(..), Board)
 import Game exposing (Game, Player(..))
 
 createNewGame : Size -> (Player, Player) -> Game
@@ -14,3 +15,12 @@ createTieGameStandardSizedBoard =
         moves = [1, 0, 4, 2, 5, 3, 6, 7, 8]
     in
         List.foldl Game.makeMove newGame moves
+
+createDrawBoardStandardSized : Board
+createDrawBoardStandardSized =
+    let
+        newBoard = Board.create Standard
+        moves = [(1, X), (0, O), (4, X), (2, O),
+                 (5, X), (3, O), (6, X), (7, O), (8, X)]
+    in
+        List.foldl Board.markCell newBoard moves
