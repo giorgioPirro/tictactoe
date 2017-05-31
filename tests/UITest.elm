664042362a93ff6f8a8cecd2f2279f580575335d
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-05-31 15:54:45 +0100

    Add fuzzy testing generating random game status

--- tests/UITest.elm
+++ tests/UITest.elm
@@ -1,59 +1,96 @@
-module UITest exposing (uiTests)
+module UITest exposing (defaultBoardRenderingTests, inPlayBoardRenderingTests)
 
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe, test, todo, fuzz)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (text, class)
 import Expect
+import Random.Pcg
+import Fuzz
+import Shrink
 
 import Board exposing (Size(..), Mark(..))
+import Game exposing (Status(..))
 import Main exposing (renderBoard)
 
-uiTests : Test
-uiTests =
-    describe "The UI should"
-        [ describe "render the board"
-            [ test "displaying as many cells as there are in the board" <|
-                  \() ->
+defaultBoardRenderingTests : Test
+defaultBoardRenderingTests =
+    describe "Regardless of Game Status"
+        [ describe "the UI should render the board"
+            [ fuzz randomGameStatus "displaying as many cells as there are in the board" <|
+                  \status->
                       Board.create Standard
-                          |> renderBoard
+                          |> renderBoard status
                           |> Query.fromHtml
                           |> Query.findAll [class "cell"]
                           |> Query.count (Expect.equal 9)
 
-            , test "arranging the cells in as many rows as there are in the board" <|
-                  \() ->
+            , fuzz randomGameStatus "arranging the cells in as many rows as there are in the board" <|
+                  \status ->
                       Board.create Standard
-                          |> renderBoard
+                          |> renderBoard status
                           |> Query.fromHtml
                           |> Query.findAll [class "row"]
                           |> Query.count (Expect.equal 3)
 
-            , test "displaying empty cells with the appropriate class" <|
-                  \() ->
+            , fuzz randomGameStatus "displaying empty cells with the appropriate class" <|
+                  \status ->
                       Board.create Standard
-                          |> renderBoard
+                          |> renderBoard status
                           |> Query.fromHtml
                           |> Query.findAll [class "cell"]
                           |> Query.each (Query.has [class "empty"])
 
-            , test "displaying crosses' cells with the appropriate class" <|
-                  \() ->
+            , fuzz randomGameStatus "displaying crosses' cells with the appropriate class" <|
+                  \status ->
                       Board.create Standard
                           |> Board.markCell (3, X)
-                          |> renderBoard
+                          |> renderBoard status
                           |> Query.fromHtml
                           |> Query.findAll [class "cell"]
                           |> Query.index 3
                           |> Query.has [class "crosses"]
 
-            , test "displaying noughts' cells with the appropriate class" <|
-                  \() ->
+            , fuzz randomGameStatus "displaying noughts' cells with the appropriate class" <|
+                  \status ->
                       Board.create Standard
                           |> Board.markCell (5, O)
-                          |> renderBoard
+                          |> renderBoard status
                           |> Query.fromHtml
                           |> Query.findAll [class "cell"]
                           |> Query.index 5
                           |> Query.has [class "noughts"]
             ]
         ]
+
+inPlayBoardRenderingTests : Test
+inPlayBoardRenderingTests =
+    describe "When the game is Ongoing"
+        [ describe "the UI should render the board"
+            [ todo "with the appropriate commands"
+            ]
+        ]
+
+
+
+
+----HELPER FUZZER
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+intToStatus : Int -> Status
+intToStatus statusId =
+    case statusId of
+        0 -> Ongoing
+        1 -> Tie
+        2 -> Win X
+        _ -> Win O
+
+randomStatusGenerator : Random.Pcg.Generator Status
+randomStatusGenerator =
+   Random.Pcg.map intToStatus (Random.Pcg.int 0 3)
+
+randomGameStatus : Fuzz.Fuzzer Status
+randomGameStatus =
+    Fuzz.custom randomStatusGenerator Shrink.noShrink
+----HELPER FUZZER
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
