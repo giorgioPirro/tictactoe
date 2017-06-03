536dbf12ff1024e2108b381974989d84215e1be4
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-06-03 09:56:08 +0100

    Fix indentation in OutcomeTest

--- tests/UI/OutcomeTest.elm
+++ tests/UI/OutcomeTest.elm
@@ -12,37 +12,37 @@ import Board exposing (Mark(..))
 outcomeTests : Test
 outcomeTests =
   describe "The outcome box"
-          [ describe "when the game is ongoing"
-              [ test "does not show anything" <|
-                    \() ->
-                        renderGameStatus Ongoing
-                            |> Query.fromHtml
-                            |> Query.children [class "outcome-message"]
-                            |> Query.count (Expect.equal 0)
-              ]
+     [ describe "when the game is ongoing"
+         [ test "does not show anything" <|
+               \() ->
+                   renderGameStatus Ongoing
+                       |> Query.fromHtml
+                       |> Query.children [class "outcome-message"]
+                       |> Query.count (Expect.equal 0)
+         ]
 
-          , describe "when the game has come to a tie"
-              [ test "displays the appropriate tie message" <|
-                    \() ->
-                        renderGameStatus Tie
-                            |> Query.fromHtml
-                            |> Query.find [class "outcome-message"]
-                            |> Query.has [text "It was a tie!!"]
-              ]
+     , describe "when the game has come to a tie"
+         [ test "displays the appropriate tie message" <|
+               \() ->
+                   renderGameStatus Tie
+                       |> Query.fromHtml
+                       |> Query.find [class "outcome-message"]
+                       |> Query.has [text "It was a tie!!"]
+         ]
 
-          , describe "when there is a winner"
-              [ test "displays the appropriate tie message (X)" <|
-                    \() ->
-                        renderGameStatus (Win X)
-                            |> Query.fromHtml
-                            |> Query.find [class "outcome-message"]
-                            |> Query.has [text "X has won!!"]
+     , describe "when there is a winner"
+         [ test "displays the appropriate tie message (X)" <|
+               \() ->
+                   renderGameStatus (Win X)
+                       |> Query.fromHtml
+                       |> Query.find [class "outcome-message"]
+                       |> Query.has [text "X has won!!"]
 
-              , test "displays the appropriate tie message (O)" <|
-                    \() ->
-                        renderGameStatus (Win O)
-                            |> Query.fromHtml
-                            |> Query.find [class "outcome-message"]
-                            |> Query.has [text "O has won!!"]
-              ]
-          ]
+         , test "displays the appropriate tie message (O)" <|
+               \() ->
+                   renderGameStatus (Win O)
+                       |> Query.fromHtml
+                       |> Query.find [class "outcome-message"]
+                       |> Query.has [text "O has won!!"]
+         ]
+     ]
