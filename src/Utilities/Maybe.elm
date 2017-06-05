6e882dc0af3872c257ea4a77a1c4bb2fccbfdbc2
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-06-05 19:36:20 +0100

    Rename variable in Utils.Maybe

--- src/Utilities/Maybe.elm
+++ src/Utilities/Maybe.elm
@@ -1,7 +1,7 @@
 module Utilities.Maybe exposing (flatMaybe)
 
 flatMaybe : Maybe (Maybe a) -> Maybe a
-flatMaybe mx =
-    case mx of
-          Just x -> x
+flatMaybe maybeSomething =
+    case maybeSomething of
+          Just something -> something
           Nothing -> Nothing
