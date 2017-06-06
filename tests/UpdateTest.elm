a9648f6596f7d4295d8f7582364ff8e73892728d
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-06-06 10:19:17 +0100

    Add more tests around Main update

--- tests/UpdateTest.elm
+++ tests/UpdateTest.elm
@@ -1,16 +1,17 @@
 module UpdateTest exposing (updateTests)
 
-import Test exposing (Test, describe, test)
+import Test exposing (Test, describe, test, fuzz)
 import Expect
 import Task exposing (Task)
+import Array
 
-import Helpers exposing (createNewGame)
-
-
+import Helpers exposing (createNewGame, randomBoardSize, randomGameType)
 import Msg exposing (Msg(..))
 import Main exposing (Model, update)
 import Board exposing (Mark(..), Size(..))
+import Game
 import Player exposing (Player(..))
+import GameGenerator exposing (GameType(..))
 
 updateTests : Test
 updateTests =
@@ -22,7 +23,35 @@ updateTests =
                         |> Tuple.second
                         |> Expect.equal Cmd.none
 
-            , Test.todo "have a move command when the next player to make a move is of Computer nature"
+            , test "update the board with the given move" <|
+                \() ->
+                    update (MakeMove 0) {game = (createNewGame Standard ((Human X), (Human O)))}
+                        |> Tuple.first
+                        |> .game
+                        |> Game.getBoard
+                        |> Board.toArray
+                        |> Array.get 0
+                        |> Expect.equal (Just (Just X))
+            ]
+
+        , describe "given a NewGame message should"
+            [ fuzz randomBoardSize "set the game to the size requested" <|
+                  \aBoardSize ->
+                      update (NewGame aBoardSize HumanVsHuman) {game = (createNewGame Standard ((Human X), (Human O)))}
+                          |> Tuple.first
+                          |> .game
+                          |> Game.getBoard
+                          |> Board.size
+                          |> Expect.equal aBoardSize
+
+            , fuzz randomGameType "set the game to the type requested" <|
+                  \aGameType ->
+                      update (NewGame Standard aGameType) {game = (createNewGame Standard ((Human X), (Human O)))}
+                          |> Tuple.first
+                          |> .game
+                          |> Game.getPlayers
+                          |> GameGenerator.whichGameType
+                          |> Expect.equal aGameType
             ]
         ]
 
